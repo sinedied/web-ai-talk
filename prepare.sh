cae835084cee7d90be28944696113c3798e8a45b
sinedied <noda@free.fr>
2026-03-25 13:45:56 +0100

    add prepare script

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Preparing demo environment..."
+
+# Common preparation for all demos
+
+# echo "" > ./genaisrc/issue-review.genai.js
+
+## Open files
+# code ./genaisrc/issue-review.genai.js
+
+echo "Setup done"
