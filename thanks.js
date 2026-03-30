bed5ff9fe1e81fbfd08ab9c55081241508cdab2b
sinedied <noda@free.fr>
2026-03-30 13:26:59 +0200

    feat: add speakers list to thank you page

---
+++ thanks.js
@@ -0,0 +1,13 @@
+// THANK YOU!
+const speakers = [
+  {
+    name: "Olivier Leplus",
+    job: "Developer Advocate", // AWS
+    linkedin: "/in/olivierleplus/",
+  },
+  {
+    name: "Yohan Lasorsa",
+    job: "Developer Advocate", // Microsoft
+    linkedin: "/in/yohanlasorsa/",
+  },
+];
